f419d66fcf25100c041ca3ee19ec0c9f6fee4793
juergen <becker@swa.asl>
2024-09-19 15:42:01 +0200

    geändert:       admin.py

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Starter, Log, LastLog
  
 # registering the model
-#admin.site.register(Starter)
+# admin.site.register(Starter)
 admin.site.register(Log)
 admin.site.register(LastLog)
 
